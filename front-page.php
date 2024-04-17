4c6ed94a8758960907e0de1f0d3aea67f5a74c56
b2me-dev <websites@b2me.marketing>
2024-04-17 10:39:46 +1000

    Clean front-page.php

--- front-page.php
+++ front-page.php
@@ -2,15 +2,9 @@
 	get_header();
 ?>
 	<main id="primary" class="site-main">
-		<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
-				endwhile;
-				the_posts_navigation();
-			endif;
-		?>
+		<div class="entry-content">
+
+		</div>
 	</main>
 <?php
 	get_footer();
